f5ebc786a53c100382ec41fc6899f8eb9f032998
Khai Nguyen <khainm@digidinos.com>
2022-02-25 23:26:36 -0500

    initial commit

---
+++ array_data.js
@@ -0,0 +1,172 @@
+let array_details = [
+	{
+		name: 'Wishbone',
+		type: 'Dog',
+		breed: 'American Bulldog Cross',
+		second_breed: '',
+		sex: 'Male',
+		color: 'Black',
+		spayed_neutered: 'Yes',
+		age: '2Yrs 1Mths 2Wks (approx)',
+		size: 'Large',
+		status: 'My health has been checked\nMy vaccinations are up to date\nMy worming is up to date\nI have been microchipped',
+		identification: {
+			id: '20579',
+			microchip_num: '985113004709947'
+		},
+		suitablity_guide: '',
+		more_info: `Wishbone is a super sweet playful guy! He is around a 2 years old, but is still very much a puppy. 
+He loves people and he, like any youngster, will need basic manner training and a patient home 
+willing to put their time and energy into him. Wishbone also seems to play well with other dogs! 
+His perfect doggy pal would be one that can keep up yet be tolerant of his playful antics. He would 
+benefit from a home willing to exhaust his energy and keep him active . Wishbone might jump on you 
+or play mouth because he is young and still learning. Wishbone will require a home with children 13 
+years and older because he gets so playful and excited he doesn't realize his size and strength. He 
+truly is a smart young man just needs the right home to spoil him rotten! My past owner went 
+through the process of getting me registered as an Emotional Support Animal as well! And I have the 
+Certificate to prove it!`,
+		place: {
+			description: 'I am at the Kenton County Animal Shelter location',
+			phone: '(859) 356-7400',
+			address: '1020 Mary Laidley Ft Mitchell, KY 41017'
+		}
+	},
+	{
+		name: 'Benny',
+		type: 'Dog',
+		breed: 'Australian Sphered Cross',
+		second_breed: '',
+		sex: 'Male',
+		color: 'Brown/White',
+		spayed_neutered: 'Yes',
+		age: '2Yrs 1Mths 4Wks (approx)',
+		size: '',
+		status: 'My health has been checked\nMy vaccinations are up to date\nMy worming is up to date\nI have been microchipped',
+		identification: {
+			id: '23531',
+			microchip_num: '985113005872810'
+		},
+		suitablity_guide: 'Not suitable for children',
+		more_info: '',
+		place: {
+			description: 'I am at the Kenton County Animal Shelter location',
+			phone: '(859) 356-7400',
+			address: '1020 Mary Laidley Ft Mitchell, KY 41017'
+		}
+	},
+	{
+		name: 'Corneila',
+		type: 'Dog',
+		breed: 'Boxer Cross',
+		second_breed: '',
+		sex: 'Female',
+		color: 'White',
+		spayed_neutered: 'Yes',
+		age: '6Yrs 2Mths 3Wks (approx)',
+		size: '',
+		status: 'My health has been checked\nMy vaccinations are up to date\nMy worming is up to date\nI have been microchipped',
+		identification: {
+			id: '23390',
+			microchip_num: '985113005872687'
+		},
+		suitablity_guide: 'Ok with Dos\nLaid back\nShy',
+		more_info: `A Little Bit About Me
+Cornelia is calm and quiet in their kennel. She needs a home willing to use positive reinforcement 
+training to build their confidence. Cornelia is shy on first meeting but if you give her time they 
+warm up quickly. She would so best in a calm quiet home due to their personality and demeanor.
+
+Cornelia seems to do well with other dogs and would love a buddy to play with in their new home. We 
+would suggest laid back, calm dogs in their new home. They are so low maintenance and calm, a 
+hyper, playful dog might stress them out.
+
+We would suggest other dogs and/or children meet them prior to being adopted because we want to 
+make sure they do not overwhelm him. In time with TLC, patience, and confidence building they  t 
+perfectly into their forever family.`,
+		place: {
+			description: 'I am at the Kenton County Animal Shelter location',
+			phone: '(859) 356-7400',
+			address: '1020 Mary Laidley Ft Mitchell, KY 41017'
+		}
+	},
+	{
+		name: 'Niko',
+		type: 'Kitten',
+		breed: 'Domestic Longhair Cross',
+		second_breed: '',
+		sex: 'Male',
+		color: 'Grey Tabby/White',
+		declawed: 'Yes',
+		spayed_neutered: 'Yes',
+		age: '3Mths 3Wks',
+		size: '',
+		status: 'My health has been checked\nMy vaccinations are up to date\nMy worming is up to date\nI have been microchipped',
+		identification: {
+			id: '23541',
+			microchip_num: '985113006299812'
+		},
+		suitablity_guide: '',
+		more_info: '',
+		place: {
+			description: 'I am in foster care, please contact Kenton County Animal Shelter for more details. You can contact me by',
+			phone: '(859) 356-7400',
+			address: '1020 Mary Laidley Ft Mitchell, KY 41017'
+		}
+	},
+	{
+		name: 'Howard',
+		type: 'Cat',
+		breed: 'Domestic Short Hair Cross',
+		second_breed: 'Russian',
+		sex: 'Male',
+		color: 'Grey',
+		declawed: 'No',
+		spayed_neutered: 'Yes',
+		age: 'Adult',
+		size: '',
+		status: 'My health has been checked\nMy vaccinations are up to date\nMy worming is up to date\nI have been microchipped',
+		identification: {
+			id: '23882',
+			microchip_num: '985113006299853'
+		},
+		suitablity_guide: '',
+		more_info: '',
+		place: {
+			description: 'I am at the Kenton County Animal Shelter location',
+			phone: '(859) 356-7400',
+			address: '1020 Mary Laidley Ft Mitchell, KY 41017'
+		}
+	},
+	{
+		name: 'Hemingway',
+		type: 'Cat',
+		breed: 'Domestic Short Hair Cross',
+		second_breed: '',
+		sex: 'Male',
+		color: 'Grey',
+		declawed: 'No',
+		spayed_neutered: 'Yes',
+		age: '3Yrs 3Wks (approx)',
+		size: '',
+		status: 'My health has been checked\nMy vaccinations are up to date\nMy worming is up to date',
+		identification: {
+			id: '23897',
+			microchip_num: ''
+		},
+		suitablity_guide: "Doesn't Like Cats\nSpecial Needs",
+		more_info: '',
+		place: {
+			description: 'I am at the Kenton County Animal Shelter location',
+			phone: '(859) 356-7400',
+			address: '1020 Mary Laidley Ft Mitchell, KY 41017'
+		}
+	}
+]
+
+let images = [
+	['images/wishbone_1.jpeg', 'images/wishbone_2.jpeg'],
+	['images/benny_1.jpeg'],
+	['images/corneila_1.jpeg'],
+	['images/niko_1.jpeg'],
+	['images/howard_1.jpeg'],
+	['images/hemingway_1.jpeg']
+]
